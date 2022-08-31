d1dcec5fca33f88285c7d4f9483f08c87193c51c
Manish Sahani <101914715+Manish0045@users.noreply.github.com>
2022-08-31 20:29:05 +0530

    File
    Portfolio site using Bootstrap and Sass

---
+++ app.js
@@ -0,0 +1,44 @@
+const sections=document.querySelectorAll('.section');
+const sectBtns=document.querySelectorAll('.controlls');
+const sectBtn=document.querySelectorAll('.control');
+const allSections=document.querySelectorAll('.main-content');
+
+function PageTransitions(){
+    //Active buttons
+    for(let i=0;i<sectBtn.length;i++){
+        sectBtn[i].addEventListener('click',function(){
+            let currentBtn=document.querySelectorAll('.active-btn');
+            currentBtn[0].className=currentBtn[0].className.replace('active-btn','');
+            this.className +='active-btn';
+        })
+    }
+
+    allSections[0].addEventListener('click',(e)=>{
+        const id=e.target.dataset.id;
+        if(id){
+            sectBtn.forEach((btn)=>{
+                btn.classList.remove('active')
+            })
+            e.target.classList.add('active')
+            sections.forEach((section)=>{
+                section.classList.remove('active');
+            })
+            const element=document.getElementById(id);
+            element.classList.add('active');
+        }
+    })
+    //toggle theme
+    const themeBtn=document.querySelector('.theme-btn');
+    themeBtn.addEventListener('click',()=>{
+        let element=document.body;
+        element.classList.toggle('light-mode');
+    })
+
+
+
+    
+}
+
+PageTransitions();
+
+
